597f4583d65ba3ffa47604d6a46b5b6b8546fbdd
Kaleab Samuel <thekaleabsamuel@gmail.com>
2024-02-26 13:54:40 -0700

    adds myfile.md

--- index.js
+++ index.js
@@ -1,11 +1,144 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+//   // Write your code here!\\
+if (feet <= 400) {
+  return "This one is on me!" 
+}
+   else if (feet > 400 && feet <= 2000){
+    return "That will be twenty bucks."
+   } else if (feet > 2000 && feet <= 2500){
+      return "I will gladly take your thirty bucks."
+    } else {
+      return "No can do."
+    }
+    }
+
+
+    // function ternaryCheckCity(city){
+    //   return city === "NYC" ? "Ok, sounds good." : "No go.";
+    // }
+    
+
+    function ternaryCheckCity(city){
+      return city === "NYC" ? "Ok, sounds good." : "No go.";
+    
+    }
+
+
+// function switchOnCharmFromTip(tip){
+//   // Write your code here!
+//   switch (tip) {
+//     case "generous":
+//       return "Thank you so much.";
+//      case "regular" :
+//       return "Thank you.";
+//       default:
+//         return "Bye.";
+//   }
+// }
+
+
+
+// function switchOnCharmFromTip(tip){
+//   switch (tip) {
+//     case "generous":
+//       return "Thank you so much.";
+//     case "regular":
+//       return "Thank you.";
+//     default:
+//       return "Bye.";
+//   }
+// }
+
+// function switchOnCharmFromTip(tip){
+//   switch (tip) {
+//     case "generous":
+//       return "Thank you so much.";
+//     case "regular":
+//       return "Thank you.";
+//     default:
+//       return "Bye.";
+//   }
+// }
+
+
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case "generous":
+      return "Thank you so much.";
+    case "not as generous":
+      return "Thank you.";
+    case "thanks for everything":
+      return "Bye.";
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+
+
+
+
+
+// function switchOnCharmFromTip(tip){
+//   switch (tip) {
+//     case "generous":
+//       return "Thank you so much.";
+//     case "regular":
+//       return "Thank you.";
+//     default:
+//       return "Thank y";
+//   }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function scuberGreetingForFeet(feet){
+//   if (feet <= 400) {
+//     return "This one is on me!";
+//   } else if (feet > 2000 && feet <= 2500) {
+//     return "I will gladly take your thirty bucks.";
+//   } else {
+//     return "No can do.";
+//   }
+// }
+
+
+
+// function scuberGreetingForFeet (feet) {
+//   if (feet <= 400) {
+//     return "This one is on me!";
+//   } else if (feet > 2000 && feet <= 2500) {
+//     return "I will gladly take your thirty bucks.";
+//   // } if (feet < 2500)
+//   } else {
+//   return "No can do.";
+//   }
+// }
+
+
+// function scuberGreetingForFeet(feet){
+//   if (feet <= 400) {
+//     return "This one is on me!";
+//   } else if (feet > 2000 && feet <= 2500) {
+//     return "I will gladly take your thirty bucks.";
+//   } else if {
+//     return "No can do.";
+//   }
+// }
